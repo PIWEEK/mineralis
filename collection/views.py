465bd9f6715c392b5c3a0fe7075a044f41ce8b45
Esther Moreno <esther.moreno@kaleidos.net>
2015-07-16 16:49:48 +0200

    yami is the best!!!!!

--- collection/views.py
+++ collection/views.py
@@ -1,5 +1,5 @@
 from django.shortcuts import render
-from .models import Mineral
+from .models import Mineral, KINDS
 
 def mineral_list(request):
     minerals = Mineral.objects.all().order_by('name')
@@ -7,30 +7,34 @@ def mineral_list(request):
         'minerals': minerals
     }
 
-    template = 'collection/kind.html'
+    template = 'collection/list.html'
     if 'd' in request.GET and request.GET.get('d') == 'mosaic':
         template = 'collection/mosaic.html'
 
     if 'k' in request.GET:
-        context['minerals'] = minerals.filter(kind=request.GET.get('k'))
-        context['kind'] = request.GET.get('k')
+        kind = request.GET.get('k')
+        context['minerals'] = minerals.filter(kind=kind)
+        context['kind'] = kind
+        context['kind_desc'] = dict(KINDS)[kind] 
 
     return render(request, template, context)
 
 def mineral_detail(request, mineral_id):
+    context = {}
     minerals = Mineral.objects.all().order_by('name')
+
     if 'k' in request.GET and request.GET.get('k') != None:
         minerals = minerals.filter(kind=request.GET.get('k'))
+        context['kind'] = request.GET.get('k')
+
     mineral = minerals.get(pk=mineral_id)
     minerals_ids = list(minerals.values_list('id', flat = True))
     mineral_ind = minerals_ids.index(int(mineral_id))
     mineral_prev = mineral_ind > 0 and minerals_ids[mineral_ind - 1] or None
     mineral_next = mineral_ind < len(minerals) -1 and minerals_ids[mineral_ind + 1] or None
 
-    context = {
-        'mineral': mineral,
-        'mineral_prev': mineral_prev,
-        'mineral_next': mineral_next
-    }
+    context['mineral'] = mineral
+    context['mineral_prev'] = mineral_prev
+    context['mineral_next'] = mineral_next
 
     return render(request, 'collection/detail.html', context)
