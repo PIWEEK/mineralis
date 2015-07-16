f332a95d158ece8e0e82acb909c8c903ce141fbc
Yamila Moreno <yamila.ms@gmail.com>
2015-07-16 16:14:05 +0200

    Add kind to filters

--- collection/views.py
+++ collection/views.py
@@ -7,22 +7,25 @@ def mineral_list(request):
         'minerals': minerals
     }
 
-    if 'd' in request.GET:
-        if request.GET.get('d') == 'mosaic':
-            return render(request, 'collection/mosaic.html', context)
-        if request.GET.get('d') == 'kind':
-            context['minerals'] = minerals.order_by('kind', 'name')
-            return render(request, 'collection/kind.html', context)
+    template = 'collection/kind.html'
+    if 'd' in request.GET and request.GET.get('d') == 'mosaic':
+        template = 'collection/mosaic.html'
 
-    return render(request, 'collection/list.html', context)
+    if 'k' in request.GET:
+        context['minerals'] = minerals.filter(kind=request.GET.get('k'))
+        context['kind'] = request.GET.get('k')
+
+    return render(request, template, context)
 
 def mineral_detail(request, mineral_id):
     minerals = Mineral.objects.all().order_by('name')
+    if 'k' in request.GET and request.GET.get('k') != None:
+        minerals = minerals.filter(kind=request.GET.get('k'))
     mineral = minerals.get(pk=mineral_id)
     minerals_ids = list(minerals.values_list('id', flat = True))
     mineral_ind = minerals_ids.index(int(mineral_id))
     mineral_prev = mineral_ind > 0 and minerals_ids[mineral_ind - 1] or None
-    mineral_next = mineral_ind < len(minerals) and minerals_ids[mineral_ind + 1] or None
+    mineral_next = mineral_ind < len(minerals) -1 and minerals_ids[mineral_ind + 1] or None
 
     context = {
         'mineral': mineral,
