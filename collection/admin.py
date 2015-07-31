c8c1fdd3afac78ff8e8998c88721b26111ed8314
Yamila Moreno <yamila.ms@gmail.com>
2015-07-31 18:01:00 +0200

    Add view-on-site link from admin

--- collection/admin.py
+++ collection/admin.py
@@ -1,4 +1,5 @@
 from django.contrib import admin
+from django.core.urlresolvers import reverse
 
 from .models import Mineral, Image
 
@@ -9,6 +10,9 @@ class MineralAdmin(admin.ModelAdmin):
     inlines = [
             ImageAdmin
     ]
-   
+
+    def view_on_site(self, obj):
+        return reverse('detail', kwargs={'mineral_id': obj.id})
+
 
 admin.site.register(Mineral, MineralAdmin)
